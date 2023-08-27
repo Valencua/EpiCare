d5b878173a0b8c5cedb8ac80668b46354fe6c4bb
Valencua <valencuavasula@gmail.com>
2023-08-27 18:03:37 -0300

    Navbar

--- workspace/Sitio/html/main.js
+++ workspace/Sitio/html/main.js
@@ -4,6 +4,19 @@ $('.message a').click(function(){
  $('.registrado').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+
+ const input = document.getElementById ("password")
+ const check = document.getElementById ("ver")
+
+ check.addEventListener("click", ()=>{
+   if(check.checked)
+   {
+      input.type = "text"
+   }
+   else{
+      input.type = "password"
+   }
+ })
  
  
  

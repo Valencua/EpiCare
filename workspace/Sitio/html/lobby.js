8a4db58072063bc9f0bc047d4931ed5af80e2d8a
Valencua <110689742+Valencua@users.noreply.github.com>
2023-09-12 10:46:31 -0300

    Vinculos

--- workspace/Sitio/html/lobby.js
+++ workspace/Sitio/html/lobby.js
@@ -1,9 +1,18 @@
 const botonOcultar = document.getElementById('cerrar');
 const miCuadro = document.getElementById('cuadro');
 const miCaja = document.getElementById('caja');
+const botonMostrar = document.getElementById('abrir')
+const miCuadro1 = document.getElementById('cuadro1');
+const miCaja1 = document.getElementById('caja1');
 
 botonOcultar.addEventListener("click", () =>{
    console.log("cerrado");
-   miCuadro.style.display = 'none';
-   miCaja.style.display = 'none';
+   miCuadro.style.visibility = 'hidden';
+   miCaja.style.visibility = 'hidden';
+})
+
+botonMostrar.addEventListener("click", () =>{
+   console.log("abierto");
+   miCuadro1.style.visibility = 'visible';
+   miCaja1.style.visibility = 'visible';
 })

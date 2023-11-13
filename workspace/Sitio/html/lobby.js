3cc5e526ae424bdf2f1de92143ad3925cb884b61
Valencua <110689742+Valencua@users.noreply.github.com>
2023-11-13 09:02:36 -0300

    Profile + perro inicio + About Us

--- workspace/Sitio/html/lobby.js
+++ workspace/Sitio/html/lobby.js
@@ -4,11 +4,13 @@ const miCaja = document.getElementById('caja');
 const botonMostrar = document.getElementById('abrir')
 const miCuadro1 = document.getElementById('cuadro1');
 const miCaja1 = document.getElementById('caja1');
+const perrito = document.getElementById('perritobusca');
 
 botonOcultar.addEventListener("click", () =>{
    console.log("cerrado");
    miCuadro.style.visibility = 'hidden';
    miCaja.style.visibility = 'hidden';
+   perrito.style.visibility = 'visible';
 })
 
 botonMostrar.addEventListener("click", () =>{

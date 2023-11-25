e0abe3cb0dcb6b70e3bf4ed0f0365ebd6f50ecfb
Valencua <valencuavasula@gmail.com>
2023-11-25 19:43:17 -0300

    Calendarios

--- workspace/Sitio/js/calendar.js
+++ workspace/Sitio/js/calendar.js
@@ -30,22 +30,35 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateCalendar(year, month) {
         calendarBody.innerHTML = "";
         calendarTitle.textContent = `${getMonthName(month)} ${year}`;
-
+    
         const daysInMonth = new Date(year, month + 1, 0).getDate();
-
+        const lastDayOfMonth = new Date(year, month, daysInMonth).getDay();
+    
         let dayCounter = 1;
         for (let i = 0; i < 6; i++) {
+            if (i === 5 && dayCounter > daysInMonth) {
+                break;
+            }
+    
             const row = document.createElement("tr");
             for (let j = 0; j < 7; j++) {
                 const cell = document.createElement("td");
                 if (dayCounter <= daysInMonth) {
-                    cell.textContent = dayCounter;
+                    const dayNumber = document.createElement("span");
+                    dayNumber.textContent = dayCounter;
+                    cell.appendChild(dayNumber);
                     dayCounter++;
                 }
                 row.appendChild(cell);
             }
             calendarBody.appendChild(row);
         }
+
+        // Añadir la clase 'empty' a los casilleros vacíos
+        const emptyCells = document.querySelectorAll(".calendar td:not(:has(span))");
+        emptyCells.forEach((cell) => {
+            cell.classList.add("empty");
+        });
     }
 
     function getMonthName(month) {
